9c45bcdab3cb5575ddd7d67bd4327ccedbc83923
Johnathan Booy <johnny_music@icloud.com>
2022-10-18 08:54:11 -0700

    Create User Model

---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    app.app_context().push()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __repr__(self):
+        return f"<User {self.id} '{self.full_name}'>"
---
+++ seed.py
@@ -0,0 +1,18 @@
+from app import app
+from models import db, User
+
+db.drop_all()
+db.create_all()
+
+
+users = [
+    User(username="bonsaibooy", password="littletrees",
+         email="johnathan.booy@gmail.com", first_name="Johnathan", last_name="Booy"),
+    User(username="pumpkinspice", password="everythingnice",
+         email="mackay.meganlea@gmail.com", first_name="Megan", last_name="Mackay")
+]
+
+for user in users:
+    db.session.add(user)
+
+db.session.commit()
